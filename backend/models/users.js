ecca02528c351ba9ac5164b8ba17b6b6276b739b
Mahima Hemachandra <nomad.inof@gmail.com>
2023-01-24 17:35:12 +0530

    pipeline creation

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require("crypto");
 
 const UserSchema = mongoose.Schema({
   userID: {
@@ -13,12 +14,38 @@ const UserSchema = mongoose.Schema({
   userEmail: {
     type: String,
     require: true,
+    unique: true
   },
-  userPassword: {
-    type: password,
-    require: true,
-  }
-});
+  enc_password: {
+    type: String,
+    require: true
+  },
+  salt: String,
+},{timestamps: true});
+
+UserSchema.virtual("password")
+  .set(function() {
+
+  })
+  .get(function() {
+
+  })
+
+UserSchema.methods = {
+  authenticate: function(plainPassw) {
+    return this.securePassw(plainPassw) === this.enc_password
+  },
+
+  securePassw: function(plainPassw) {
+    if(!plainPassw) return "";
+
+    try {
+      return crypto.createHmac("sha256", this.salt).update(plainPassw).digest('hex')
+    } catch (err) {
+      return ""
+    }
+  },
+}
 
 const user = mongoose.model("users", UserSchema);
 module.exports = user;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
-
+//MongoDB connection
 mongoose.connect('mongodb+srv://mahima:5688699@cluster0.cb9rhah.mongodb.net/Car-rental?retryWrites=true&w=majority', (err)=> {
   if (!err)
     console.log('MongoDB successfully connected!');
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://mahima:5688699@cluster0.cb9rhah.mongodb.net/Car-
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
